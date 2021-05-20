aa5f51b55ed03cb2dd8a13894d17680d5bd17528
Dvir Farkash 2 <dviref09@gmail.com>
2021-05-20 16:18:53 +0300

    first commit

---
+++ basic.py
@@ -0,0 +1,34 @@
+###################################
+#             TOKENS              #
+###################################
+
+TT_INT		= 'INT'
+TT_FLOAT    = 'FLOAT'
+TT_PLUS     = 'PLUS'
+TT_MINUS    = 'MINUS'
+TT_MUL      = 'MUL'
+TT_DIV      = 'DIV'
+TT_LPAREN   = 'LPAREN'
+TT_RPAREN   = 'RPAREN'
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+    def __repr__(self):
+        if self.value: return f'{self.type}: {self.value}'
+        return f'{self.type}'
+
+##################################
+#             LEXER              #
+##################################
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = -1
+        self.current_char = None
+    
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.text[self.pos]
