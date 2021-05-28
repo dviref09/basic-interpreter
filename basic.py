76d87429916ed44850d7037e873f1cfbfe9b8e49
Dvir Farkash 2 <dviref09@gmail.com>
2021-05-28 17:54:07 +0300

    commit

--- basic.py
+++ basic.py
@@ -144,7 +144,7 @@ class Lexer:
 #             NODES              #
 ##################################
 
-class Nodes():
+class NumberNode():
     def __init__(self, tok):
         self.tok = tok
     
@@ -176,14 +176,37 @@ class Parser:
             self.current_tok = self.tokens[self.tok_idx]
         return self.current_tok
     
-    def factor():
-        pass
+    def parse(self):
+        res = self.expr()
+        return res
 
-    def term():
-        pass
+    ###################################
 
-    def expr():
-        pass
+    def factor(self):
+        tok = self.current_tok
+
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    def expr(self):
+        return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+
+    ###################################
+
+    def bin_op(self, func, ops):
+        left = func()
+
+        while self.current_tok.type in ops:
+            op_tok = self.current_tok
+            self.advance()
+            right = func()
+            left = BinOpNode(left, op_tok, right)
+        
+        return left
 
 ################################
 #             RUN              #
@@ -192,5 +215,9 @@ class Parser:
 def run(fn, text):
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
+    if error: return None, error
 
-    return tokens, error
+    # Generate AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    return ast, None
